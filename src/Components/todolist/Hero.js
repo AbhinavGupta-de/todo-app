ea08abf062c7eb2e9251f12f9018b148a0fd6df9
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-15 23:34:38 +0530

    Finished the implementation

--- src/Components/todolist/Hero.js
+++ src/Components/todolist/Hero.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './TaskList';
+import NewTask from './NewTask';
 
 const Hero = () => {
+	const [todos, setTodos] = useState([]);
+
+	const closeTask = (id) => {
+		const updatedTodos = todos.filter((todo) => todo.id !== id);
+		setTodos(updatedTodos);
+	};
+
+	const statusToggle = (id) => {
+		const updatedTodos = todos.map((todo) => {
+			if (todo.id === id) {
+				todo.status = !todo.status;
+			}
+			return todo;
+		});
+		setTodos(updatedTodos);
+	};
+
+	if (todos.length === 0)
+		return (
+			<div className="mt-20 items-center flex flex-col w-auto">
+				<div className="w-3/5">
+					<NewTask addTask={setTodos} length={todos.length} />
+				</div>
+			</div>
+		);
+
 	return (
 		<div className="mt-20 items-center flex flex-col w-auto">
-			<TaskList />
+			<div className="w-3/5">
+				<NewTask addTask={setTodos} length={todos.length} />
+			</div>
+			<TaskList
+				closeTask={closeTask}
+				statusToggle={statusToggle}
+				todos={todos}
+			/>
 		</div>
 	);
 };
--- src/Components/todolist/TaskList.js
+++ src/Components/todolist/TaskList.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-
-import NewTask from './NewTask';
+import React from 'react';
 import List from './List';
+import ListOptions from './ListOptions';
 
 // TODO Make the tasks deletable
 // TODO Make the tasks moveable
@@ -9,57 +8,21 @@ import List from './List';
 // TODO Make the tasks filterable
 // TODO Make the number of tasks visible ---> 3 tasks left...
 
-const TaskList = () => {
-	const [todos, setTodos] = useState([]);
-	const [filterSting, setFilterString] = useState('all');
-
-	const closeTask = (id) => {
-		const updatedTodos = todos.filter((todo) => todo.id !== id);
-		setTodos(updatedTodos);
-	};
-
-	const statusToggle = (id) => {
-		const updatedTodos = todos.map((todo) => {
-			if (todo.id === id) {
-				todo.status = !todo.status;
-			}
-			return todo;
-		});
-		setTodos(updatedTodos);
-	};
-
-	if (todos.length === 0)
-		return (
-			<div className="w-3/5">
-				<NewTask addTask={setTodos} length={todos.length} />
-			</div>
-		);
+const TaskList = ({ closeTask, statusToggle, todos }) => {
+	const [filterString, setFilterString] = React.useState('All');
 
 	return (
 		<div className="w-3/5">
-			<NewTask addTask={setTodos} length={todos.length} />
 			<div className="task-container">
 				<ul className="bt-1 w-full text-left">
-					{todos.map((todo) => {
-						return (
-							<List
-								todo={todo}
-								closeTask={closeTask}
-								statusToggle={statusToggle}
-							/>
-						);
-					})}
-					<li>
-						<div className="list-options">
-							<span>{todos.length} items left</span>
-							<div className="flex flex-row justify-between">
-								<button className="mr-2">All</button>
-								<button className="mr-2">Active</button>
-								<button>Completed</button>
-							</div>
-							<button>Clear Completed</button>
-						</div>
-					</li>
+					<List
+						todos={todos}
+						filterString={filterString}
+						closeTask={closeTask}
+						statusToggle={statusToggle}
+					/>
+
+					<ListOptions todos={todos} setFilterString={setFilterString} />
 				</ul>
 			</div>
 		</div>
